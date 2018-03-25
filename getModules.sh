fafd3ce021efa9424d2b034989937e33d217ab8f
enricomariacaporossi <enrico.caporossi@gmail.com>
2018-03-25 08:12:00 +0200

    Update getModules.sh

--- getModules.sh
+++ getModules.sh
@@ -5,3 +5,4 @@ git clone https://github.com/JetBrains/jdk8u_jaxws.git jaxws
 git clone https://github.com/JetBrains/jdk8u_jdk.git jdk
 git clone https://github.com/JetBrains/jdk8u_langtools.git langtools
 git clone https://github.com/JetBrains/jdk8u_nashorn.git nashorn
+git clone https://github.com/JetBrains/jdk8u_jb.git jb
